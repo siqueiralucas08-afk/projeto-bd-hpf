3d1fc382571e4b08a1ca12aeb471d9e1452a425d
siqueiralucas08-afk <siqueira.lucas08@escola.pr.gov.br>
2026-09-17 10:37:25 -0300

    novo script

--- script-bd-hpf.sql
+++ script-bd-hpf.sql
@@ -1,13 +1,5 @@
-CREATE TABLE professor(
-  id_professor SERIAL PRIMARY KEY,
-  idade date,
-  quantidade_professor int,
-  nome varchar(255),
-  fk_escola int
+CREATE TABLE turma_disciplina_professor(
+  id_turma SERIAL PRIMARY KEY,
+  id_disciplina bigint,
+  id_professor bigint
 );
-
-SELECT * FROM professor;
-
-INSERT INTO professor(idade, quantidade_professor, nome) VALUES ('2016-06-25', 15, 'Cláudio');
-
-SELECT * FROM professor;
